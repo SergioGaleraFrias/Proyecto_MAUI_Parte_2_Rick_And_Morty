78adaa1aeeacb65a906a67aa90f5b0548f5b2ba4
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-11-14 10:33:30 +0100

    commit endpoint

---
+++ EndPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_MAUI_Parte_2_Rick_And_Morty
+{
+    internal class EndPoint
+    {
+        public static string GetRMChar(string name)
+        {
+            return $"https://rickandmortyapi.com/api/character{name}";
+        }
+    }
+}
