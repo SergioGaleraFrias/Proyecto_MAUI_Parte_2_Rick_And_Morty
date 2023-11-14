e981bf24eda619fb09030ee2349a55e31f371cf4
Ander <ander.delaiglesiape@elorrieta-errekamari.com>
2023-11-14 08:45:15 +0100

    Carpetas

---
+++ Models/RickMortyModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_MAUI_Parte_2_Rick_And_Morty.Models
+{
+    internal class RickMortyModel
+    {
+    }
+}
